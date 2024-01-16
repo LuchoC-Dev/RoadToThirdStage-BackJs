9a248226601d5360b04d89eccd30aeaa47b66dc2
LuchoC-Dev <LuchoC.dev@gmail.com>
2024-01-16 19:54:40 -0300

    fix: :bug: FIX server launch

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
+import App from './class/App';
+
 async function environment() {
   try {
     const app = new App();
     app.init();
-    console.log(`Environment start success`);
+    console.log(`Environment started successfully`);
   } catch (error) {
     console.error(`Error in environment launch`);
   }
 }
+
+environment();
