18c41906c674dc6a16ea1dc6355df3cc1225f60a
LuchoC-Dev <LuchoC.dev@gmail.com>
2024-01-17 09:41:30 -0300

    fix: :bug: FIX products params

--- src/routes/api/products/products.routes.js
+++ src/routes/api/products/products.routes.js
@@ -4,9 +4,9 @@ import ProductsMdw from '../../../middlewares/api/products/ProductsMdw.js';
 const router = Router();
 const path = '/api/products/';
 
-router.get(path + ':id', ProductsMdw.checkGet, ProductsCrud.runGet);
-router.post(path + ':id', ProductsMdw.checkPost, ProductsCrud.runPost);
-router.put(path + ':id', ProductsMdw.checkPut, ProductsCrud.runPut);
-router.delete(path + ':id', ProductsMdw.checkDelete, ProductsCrud.runDelete);
+router.get(path + ':id?', ProductsMdw.checkGet, ProductsCrud.runGet);
+router.post(path + ':id?', ProductsMdw.checkPost, ProductsCrud.runPost);
+router.put(path + ':id?', ProductsMdw.checkPut, ProductsCrud.runPut);
+router.delete(path + ':id?', ProductsMdw.checkDelete, ProductsCrud.runDelete);
 
 export default router;
