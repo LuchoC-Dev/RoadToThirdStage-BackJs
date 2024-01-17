6a78231261ef054f866e124d75299a4d06029cc6
LuchoC-Dev <LuchoC.dev@gmail.com>
2024-01-16 22:06:04 -0300

    build: :building_construction: CREATE unimplements

---
+++ src/class/unimplements.js
@@ -0,0 +1,5 @@
+function unimplements(value) {
+  return `Unimplements method: ${value}`;
+}
+
+export default unimplements;
--- src/middlewares/apiProducts/crud/checkGet.js
+++ src/middlewares/apiProducts/crud/checkGet.js
@@ -1,3 +1,5 @@
+import unimplements from '../../../class/Unimplements';
+
 function checkGet(req, res, next) {
   try {
     if (!checkParams(req)) {
@@ -11,8 +13,20 @@ function checkGet(req, res, next) {
     }
     next();
   } catch (error) {
-    console.error('Error en checkGet, cambiar ' + error);
+    console.error('Error en checkGet, cambiar \n' + error);
   }
+}
+
+function checkParams(req) {
+  const { id } = req.params;
+  console.error(unimplements('checkParams'));
+  return isValidId(id);
+}
+
+function checkQuery(req) {
+  const { data } = req.query;
+  console.error(unimplements('checkQuery'));
+  return isValidData(data);
 }
 
 export default checkGet;
