03df2a6f229df787681fe0de1bd9ac575b40400a
LuchoC-Dev <LuchoC.dev@gmail.com>
2024-01-17 13:35:24 -0300

    fix: :bug: FIX database url

--- src/utils/env.js
+++ src/utils/env.js
@@ -3,4 +3,4 @@ export const APP_URL = `http://localhost:${PORT}`;
 export const DB_USER_NAME = 'test';
 export const DB_USER_PASSWORD = 'forTest';
 export const DB_NAME = 'proyectoNode';
-export const DB_URL = `mongodb+srv://${DB_USER_NAME}:${DB_USER_PASSWORD}@coderhouse-jsback.h9u1po1.mongodb.net/`;
+export const DB_URL = `mongodb+srv://${DB_USER_NAME}:${DB_USER_PASSWORD}@cluster0.jurxuwg.mongodb.net/`;
