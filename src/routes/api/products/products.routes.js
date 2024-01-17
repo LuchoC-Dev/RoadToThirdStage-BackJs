ee733e57a389b163eea38d9b1ddb2f343375a361
LuchoC-Dev <LuchoC.dev@gmail.com>
2024-01-16 21:10:05 -0300

    build: :building_construction: UPDATE products routes

--- src/routes/api/products/products.routes.js
+++ src/routes/api/products/products.routes.js
@@ -4,5 +4,8 @@ const router = Router();
 const path = '/api/products/';
 
 router.get(path + ':id', ProductsMdw.checkGet, ProductsCrud.runGet);
+router.post(path + ':id', ProductsMdw.checkPost, ProductsCrud.runPost);
+router.put(path + ':id', ProductsMdw.checkPut, ProductsCrud.runPut);
+router.delete(path + ':id', ProductsMdw.checkDelete, ProductsCrud.runDelete);
 
 export default router;
