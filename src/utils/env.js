2298042dd80530e8acf9fe8dab0d9a34f5685fff
LuchoC-Dev <LuchoC.dev@gmail.com>
2024-01-18 08:43:15 -0300

    refactor: :memo: UPDATE db url

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,6 +1,6 @@
 export const PORT = 8080;
 export const APP_URL = `http://localhost:${PORT}`;
 export const DB_USER_NAME = 'test1';
-export const DB_USER_PASSWORD = 'forTest';
+export const DB_USER_PASSWORD = 'myTest';
 export const DB_NAME = 'proyectoNode';
-export const DB_URL = `mongodb+srv://${DB_USER_NAME}:${DB_USER_PASSWORD}@cluster0.jurxuwg.mongodb.net/`;
+export const DB_URL = `mongodb+srv://${DB_USER_NAME}:${DB_USER_PASSWORD}@mycluster.3j1n6yf.mongodb.net/${DB_NAME}`;
