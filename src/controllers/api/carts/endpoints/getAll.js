a2bd0b761cf07615de30aed708b56f55a30c325c
LuchoC-Dev <LuchoC.dev@gmail.com>
2024-01-19 09:38:16 -0300

    build: :truck: COPY products endpoints to carts

---
+++ src/controllers/api/carts/endpoints/getAll.js
@@ -0,0 +1,14 @@
+import Response from '../../../../class/Response.js';
+import productsDao from '../../../../daos/ProductsDao.js';
+
+async function getAll(req, res) {
+  try {
+    const result = await productsDao.getAll();
+    res.status(200).json(Response.ok(result));
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(Response.error(error));
+  }
+}
+
+export default getAll;
