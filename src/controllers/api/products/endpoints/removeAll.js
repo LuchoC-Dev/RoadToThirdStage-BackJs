135afcf7982695c6e8554fdc8815fb1c3d8510d8
LuchoC-Dev <LuchoC.dev@gmail.com>
2024-01-18 21:33:09 -0300

    refactor: :recycle: REFACTOR products endpoints

---
+++ src/controllers/api/products/endpoints/removeAll.js
@@ -0,0 +1,19 @@
+import Response from '../../../../class/Response.js';
+import unimplements from '../../../../class/unimplements.js';
+import productsDao from '../../../../daos/ProductsDao.js';
+
+async function removeAll(req, res) {
+  try {
+    const { clear } = req.query;
+    if (clear !== 'true') {
+      throw new Error('Invalid key');
+    }
+    const result = await productsDao.delete({});
+    res.status(200).json(Response.ok(result));
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(Response.error(error));
+  }
+}
+
+export default removeAll;
